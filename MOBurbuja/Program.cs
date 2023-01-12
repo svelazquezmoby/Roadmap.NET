11f61d02b52b0ff37e1240385e5b7b61f9fef559
svelazquezmoby <svelazquez@mobydigital.com>
2023-01-12 16:10:31 -0300

    quicksort

---
+++ MOBurbuja/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace burbuja
+{
+    class program
+    {
+        static int[] numeros = new int[] { 10, 3, 5, 6, 7, 23 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio");
+            mostrar();
+           
+            for (int i = 0; i < numeros.Length; i++) 
+            {
+                
+                for (int j = 0; j < numeros.Length -i - 1; j++)//optimizacion de 42 iteraciones a 21
+                {
+                    if (numeros[j] > numeros[j+1])
+                    {
+                        
+                        int aux = numeros[j];
+                        numeros[j] = numeros[j+1];
+                        numeros[j+1] = aux;
+
+                    }
+                }
+            }
+            Console.WriteLine("ordenado");
+            mostrar();
+        }
+
+        public static void mostrar()
+        {
+            foreach(var numero in numeros)
+            {
+                Console.WriteLine(numero + " ");
+            }
+
+        }
+    }
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("longitud del vector");
+            n = Int32.Parse(Console.ReadLine());
+
+            agregar b = new agregar(n);
+        }
+        class agregar
+        {
+            int h;
+            int[] vector;
+            public agregar(int n)
+            {
+                h = n;
+                vector = new int[h];
+                for(int i = 0; i < h; i++)
+                {
+                    Console.Write("ingrese valor {0} : ",i);
+                    vector[i] = Int32.Parse(Console.ReadLine());
+                }
+
+                quicksort(vector, 0, h - 1);
+                mostrar();
+            }
+            private void quicksort(int[] vector,int primero, int ultimo)
+            {
+                int i, j, central;
+                double pivote;
+                central = (primero + ultimo)/2;
+                pivote = vector[central];
+                i = primero;
+                j = ultimo;
+                do
+                {
+                    while (vector[i] < pivote) i++;
+                    while (vector[j] > pivote) j--;
+                    if (i <= j)
+                    {
+                        int temp;
+                        temp = vector[i];   
+                        vector[i] = vector[j];
+                        vector[j] = temp;
+                        i++;
+                        j--;
+                    }
+                }while (i <= j);
+                if(primero < j)
+                {
+                    quicksort(vector, primero, j);
+                }
+                if (i<=ultimo)
+                {
+                    quicksort(vector, i, ultimo);
+                }
+            }
+            private void mostrar()
+            {
+                Console.WriteLine("vector ordenado asc");
+                for(int i=0; i<h; i++)
+                {
+                    Console.WriteLine("{0}", vector[i]);
+
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
